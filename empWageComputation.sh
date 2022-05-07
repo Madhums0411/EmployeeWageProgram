71f21c81b2c36a7d975a3b6ad1854a26fb0e8685
madhums0411 <madhums41175@gmail.com>
2022-05-07 16:41:37 +0530

    Employee status

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,13 @@
 echo "Wellcome to my employee wage program"
 
 echo "hello"
+
+ispresent=1:
+randomcheck=$(( RANDOM%2 )):
+if [ $ispresent -eq $randomcheck ]
+then
+echo "Employee is present"
+else
+echo "Employee is absent"
+fi
+
